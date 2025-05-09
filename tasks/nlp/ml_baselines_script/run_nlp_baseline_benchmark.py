ace8eb6ae527ebc327df400e38477314a9c9f86f
Abdou Karim KANDJI <karim_155@hotmail.com>
2025-05-09 21:16:02 +0000

    improve nlp benchmarks

--- tasks/nlp/ml_baselines_script/run_nlp_baseline_benchmark.py
+++ tasks/nlp/ml_baselines_script/run_nlp_baseline_benchmark.py
@@ -38,10 +38,10 @@ logger.info(f"Using {device} device")
 
 # DATASET_PATH = ".dataset/text/wolbanking77v1"
 
-def export_results_to_csv(precision, recall, f1, output_dir):
+def export_results_to_csv(precision, recall, f1, output_dir, split):
     data = {
         'Model': ['BoW+KNN', 'BoW+SVM', 'BoW+LR', 'BoW+NB'],
-        'split': ['5k', '5k', '5k', '5k'],
+        'split': ['full', 'full', 'full', 'full'],
         'Precision': precision,
         'Recall': recall,
         'F1': f1
@@ -50,9 +50,8 @@ def export_results_to_csv(precision, recall, f1, output_dir):
     results_df = pd.DataFrame(data)
 
     # Save the DataFrame to a CSV file
-    results_df.to_csv(os.path.join(output_dir, "benchmark_baseline_results_5k.csv"), index=False)
+    results_df.to_csv(os.path.join(output_dir, "benchmark_baseline_results_{}.csv".format(split=split)), index=False)
 
-    
 
 def compute_metrics(y_true, y_pred):
     logger.info("f1_score:")
@@ -67,9 +66,25 @@ def compute_metrics(y_true, y_pred):
 
     return f1, precision, recall
 
-def load_data(dataset_path):
-    df_train = pd.read_csv(os.path.join(dataset_path, "full/train/train.csv"))
-    df_test = pd.read_csv(os.path.join(dataset_path, "full/test/test.csv"))
+def load_data(dataset_path, split="full"):
+    """
+    Load the dataset from the specified path.
+    Clean the text data by removing punctuation and converting to lowercase.
+    Encode the labels using LabelEncoder.
+    Args:
+        dataset_path (str): Path to the dataset directory.
+        split (str): The split of the dataset to load. Default is "full".
+    Returns:
+        X_train (pd.Series): Cleaned training input data.
+        X_test (pd.Series): Cleaned testing input data.
+        y_train_encoded (np.ndarray): Encoded training labels.
+        y_test_encoded (np.ndarray): Encoded testing labels.
+        le (LabelEncoder): LabelEncoder object used for encoding labels.
+        num_labels (int): Number of unique labels in the dataset.
+    """
+    df_train = pd.read_csv(os.path.join(dataset_path, "{split}/train/train.csv".format(split=split)))
+    df_test = pd.read_csv(os.path.join(dataset_path, "{split}/test/test.csv".format(split=split)))
+    
     df_train = df_train[['input_wo', 'label']]
     df_test = df_test[['input_wo', 'label']]
     #removal of punctuation marks
@@ -128,7 +143,7 @@ def compute_lr(le, X_train_seq, X_test_seq, y_train_encoded, y_test_encoded):
 
 def compute_nb(le, X_train_seq, X_test_seq, y_train_encoded, y_test_encoded):
     clf = GaussianNB()
-    clf.fit(X_train_seq, y_train_encoded)
+    clf.fit(X_train_seq.toarray(), y_train_encoded)
     pred = clf.predict(X_test_seq.toarray())
     y_true = [le.classes_[y] for y in y_test_encoded]
     y_pred = [le.classes_[y] for y in pred]
@@ -150,59 +165,65 @@ def main():
     # Optional parameters
     parser.add_argument(
         "--output_dir",
-        default=".results",
+        default="results",
         type=str,
         help="The output directory where the model predictions results will be written.",
     )
     
     args = parser.parse_args()
-    # Load the dataset
-    X_train, X_test, y_train_encoded, y_test_encoded, le, num_labels = load_data(args.dataset_dir)
-    logger.info("Dataset loaded")
 
-    # Apply Bag of Words
-    X_train_seq, X_test_seq = apply_bow(X_train, X_test)
-    logger.info("Bag of Words applied")
-    f1_scores = []
-    precision_scores = []
-    recall_scores = []
-    
-    # Compute KNN
-    y_true, y_pred = compute_knn(le, X_train_seq, X_test_seq, y_train_encoded, y_test_encoded)
-    f1, precision, recall = compute_metrics(y_true, y_pred)
-    f1_scores.append(f1)
-    precision_scores.append(precision)
-    recall_scores.append(recall)
-    logger.info("KNN computed")
+    splits = ["full", "5k_split"]
+    for split in splits:
 
-    # Compute SVM
-    y_true, y_pred = compute_svm(le, X_train_seq, X_test_seq, y_train_encoded, y_test_encoded)
-    f1, precision, recall = compute_metrics(y_true, y_pred)
-    f1_scores.append(f1)
-    precision_scores.append(precision)
-    recall_scores.append(recall)
-    logger.info("SVM computed")
+        logger.info("Run {split} benchmark script")
+        
+        # Load the dataset
+        X_train, X_test, y_train_encoded, y_test_encoded, le, num_labels = load_data(args.dataset_dir, split=split)
+        logger.info("Dataset loaded")
 
-    # Compute Logistic Regression
-    y_true, y_pred = compute_lr(le, X_train_seq, X_test_seq, y_train_encoded, y_test_encoded)
-    f1, precision, recall = compute_metrics(y_true, y_pred)
-    f1_scores.append(f1)
-    precision_scores.append(precision)
-    recall_scores.append(recall)
-    logger.info("Logistic Regression computed")
+        # Apply Bag of Words
+        X_train_seq, X_test_seq = apply_bow(X_train, X_test)
+        logger.info("Bag of Words applied")
+        f1_scores = []
+        precision_scores = []
+        recall_scores = []
+        
+        # Compute KNN
+        y_true, y_pred = compute_knn(le, X_train_seq, X_test_seq, y_train_encoded, y_test_encoded)
+        f1, precision, recall = compute_metrics(y_true, y_pred)
+        f1_scores.append(f1)
+        precision_scores.append(precision)
+        recall_scores.append(recall)
+        logger.info("KNN computed")
 
-    # Compute Naive Bayes
-    y_true, y_pred = compute_nb(le, X_train_seq, X_test_seq, y_train_encoded, y_test_encoded)
-    f1, precision, recall = compute_metrics(y_true, y_pred)
-    f1_scores.append(f1)
-    precision_scores.append(precision)
-    recall_scores.append(recall)
-    logger.info("Naive Bayes computed")
+        # Compute SVM
+        y_true, y_pred = compute_svm(le, X_train_seq, X_test_seq, y_train_encoded, y_test_encoded)
+        f1, precision, recall = compute_metrics(y_true, y_pred)
+        f1_scores.append(f1)
+        precision_scores.append(precision)
+        recall_scores.append(recall)
+        logger.info("SVM computed")
 
-    # Export results to CSV
-    # os.makedirs(output_dir, exist_ok=True)
-    export_results_to_csv(precision_scores, recall_scores, f1_scores, args.ouptput_dir)
-    logger.info("Results exported to CSV")
+        # Compute Logistic Regression
+        y_true, y_pred = compute_lr(le, X_train_seq, X_test_seq, y_train_encoded, y_test_encoded)
+        f1, precision, recall = compute_metrics(y_true, y_pred)
+        f1_scores.append(f1)
+        precision_scores.append(precision)
+        recall_scores.append(recall)
+        logger.info("Logistic Regression computed")
+
+        # Compute Naive Bayes
+        y_true, y_pred = compute_nb(le, X_train_seq, X_test_seq, y_train_encoded, y_test_encoded)
+        f1, precision, recall = compute_metrics(y_true, y_pred)
+        f1_scores.append(f1)
+        precision_scores.append(precision)
+        recall_scores.append(recall)
+        logger.info("Naive Bayes computed")
+
+        # Export results to CSV
+        os.makedirs(args.output_dir, exist_ok=True)
+        export_results_to_csv(precision_scores, recall_scores, f1_scores, args.output_dir)
+        logger.info("Results exported to CSV")
 
 
 if __name__ == "__main__":
