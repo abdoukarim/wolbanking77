13b897219bfaf9af3616f942eb95b5211ba2dab7
Abdou Karim KANDJI <karim_155@hotmail.com>
2025-10-15 19:46:14 +0000

    Update kaggle dataset link

--- download_dataset.py
+++ download_dataset.py
@@ -1,6 +1,6 @@
 import os, sys
-os.environ["KAGGLE_USERNAME"] = input("Enter your Kaggle username: ")
-os.environ["KAGGLE_KEY"] = input("Enter your Kaggle key: ")
+# os.environ["KAGGLE_USERNAME"] = input("Enter your Kaggle username: ")
+# os.environ["KAGGLE_KEY"] = input("Enter your Kaggle key: ")
 
 import kagglehub
 
@@ -9,5 +9,5 @@ from utils.logger import setup_logger
 
 logger = setup_logger("Downloading dataset")
 # Download the latest version.
-source_dataset_path = kagglehub.dataset_download("anonymous/WolBanking77")
+source_dataset_path = kagglehub.dataset_download("abdoukarimkandji/WolBanking77")
 logger.info(f"Dataset downloaded to {source_dataset_path}")
